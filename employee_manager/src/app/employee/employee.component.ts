c180cb0d7ec0d9fcd6aef15eef1a9d903b1c708e
jankier-admin <jan.kierejsza@gmail.com>
2024-07-18 12:57:05 +0200

    modified employee component by adding list of emplyees and functionality that allows viewing selected emplyee details

--- employee_manager/src/app/employee/employee.component.ts
+++ employee_manager/src/app/employee/employee.component.ts
@@ -2,22 +2,20 @@ import { Component } from '@angular/core';
 import { Employee } from '../../models/employee.model';
 import { NgForOf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { EMPLOYEES } from '../../mock/employees.mock';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule, NgForOf],
+  imports: [UpperCasePipe, FormsModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.scss',
 })
 export class EmployeeComponent {
-  employee: Employee = {
-    id: '1',
-    name: 'John',
-    surname: 'Doe',
-    employment_date: '2020-02-10',
-    skills: ['TypeScript', 'Java', 'Angular'],
-    projects: ['Todo app', 'Weather app'],
-    manager: 'Jerry Smith',
-  };
+  employees: Employee[] = EMPLOYEES;
+  selectedEmployee?: Employee;
+
+  public onSelect(employee: Employee): void {
+    this.selectedEmployee = employee;
+  }
 }
