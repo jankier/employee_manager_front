f568c5ce410f2df2eb1d627849798b48aa7ddcad
jankier-admin <jan.kierejsza@gmail.com>
2024-08-08 15:20:00 +0200

    added and adjusted mediaQuery for small devices, created hamburger menu for small devices, small fixes in scss styles

--- employee_manager/src/app/shared/components/layout/header/header.component.ts
+++ employee_manager/src/app/shared/components/layout/header/header.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { LanguageSelectorComponent } from '../../language-selector/language-selector.component';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { Paths } from '../../../../../enums/paths.enum';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [LanguageSelectorComponent, RouterLink, RouterLinkActive, TranslateModule],
+  imports: [LanguageSelectorComponent, RouterLink, RouterLinkActive, TranslateModule, MatIcon, MatIconButton, MatMenu, MatMenuItem, MatMenuTrigger],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   protected readonly Paths = Paths;
+
+  smallScreenWidth: number = 860; // px
+  isSmallScreen: boolean = false;
+  screenWidth: number = 0;
+
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+    this.checkIfSmallScreen();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(): void {
+    this.screenWidth = window.innerWidth;
+    this.checkIfSmallScreen();
+  }
+
+  checkIfSmallScreen(): void {
+    this.isSmallScreen = this.screenWidth <= this.smallScreenWidth;
+  }
+
   constructor(public translate: TranslateService) {}
 }
