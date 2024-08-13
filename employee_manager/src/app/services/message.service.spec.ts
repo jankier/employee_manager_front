beb968cfff409467f551ac707d3ab95e1acf41d4
jankier-admin <jan.kierejsza@gmail.com>
2024-08-13 13:59:46 +0200

    added test for language selection for language-selector, added test for addition message to already existing array for message service

--- employee_manager/src/app/services/message.service.spec.ts
+++ employee_manager/src/app/services/message.service.spec.ts
@@ -13,17 +13,33 @@ describe('MessageService', (): void => {
     expect(service).toBeTruthy();
   });
 
-  it('should send message', (): void => {
+  it('should send message to empty', (): void => {
     const spy = spyOn(service, 'add');
     const message: string = 'Fetched all employees';
     service.add(message);
     expect(spy).toHaveBeenCalledWith(message);
   });
 
-  it('should add message', (): void => {
+  it('should add message to empty', (): void => {
     const message: string = 'Fetched all employees';
     service.add(message);
     const length: number = 1;
+    expect(service.messages).toHaveSize(length);
+  });
+
+  it('should send message to existing', (): void => {
+    service.messages = ['Added new employee of id=11', 'Deleted employee of id=10', 'Fetched random employees'];
+    const spy = spyOn(service, 'add');
+    const message: string = 'Fetched all employees';
+    service.add(message);
+    expect(spy).toHaveBeenCalledWith(message);
+  });
+
+  it('should add message to existing', (): void => {
+    service.messages = ['Added new employee of id=11', 'Deleted employee of id=10', 'Fetched random employees'];
+    const message: string = 'Fetched all employees';
+    service.add(message);
+    const length: number = 4;
     expect(service.messages).toHaveSize(length);
   });
 
--- employee_manager/src/app/shared/components/language-selector/language-selector.component.spec.ts
+++ employee_manager/src/app/shared/components/language-selector/language-selector.component.spec.ts
@@ -4,14 +4,21 @@ import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpLoaderFactory } from '../../../app.config';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatMenuHarness } from '@angular/material/menu/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('LanguageSelectorComponent', (): void => {
   let component: LanguageSelectorComponent;
   let fixture: ComponentFixture<LanguageSelectorComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async (): Promise<void> => {
     await TestBed.configureTestingModule({
-      imports: [LanguageSelectorComponent],
+      imports: [LanguageSelectorComponent, MatButtonModule, MatMenuModule, NoopAnimationsModule],
       providers: [
         provideHttpClient(),
         importProvidersFrom(
@@ -29,9 +36,38 @@ describe('LanguageSelectorComponent', (): void => {
     fixture = TestBed.createComponent(LanguageSelectorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
   it('should create', (): void => {
     expect(component).toBeTruthy();
+  });
+
+  it('language menu should open and close', async (): Promise<void> => {
+    const menu: MatMenuHarness = await loader.getHarness(MatMenuHarness.with({ selector: `[data-testid="lang-menu"]` }));
+    expect(await menu.isOpen()).toBe(false);
+    await menu.open();
+    expect(await menu.isOpen()).toBe(true);
+    await menu.close();
+    expect(await menu.isOpen()).toBe(false);
+  });
+
+  it('should get all language menu items', async (): Promise<void> => {
+    component.languages = ['en', 'pl'];
+    const menu: MatMenuHarness = await loader.getHarness(MatMenuHarness.with({ selector: `[data-testid="lang-menu"]` }));
+    await menu.open();
+    expect((await menu.getItems()).length).toBe(2);
+  });
+
+  it('should switch translate language to pl and en', async (): Promise<void> => {
+    spyOn(component, 'switchLang');
+    component.languages = ['en', 'pl'];
+    component.currentLang = 'en';
+    const menu: MatMenuHarness = await loader.getHarness(MatMenuHarness.with({ selector: `[data-testid="lang-menu"]` }));
+    await menu.open();
+    await menu.clickItem({ text: 'PL' });
+    expect(component.switchLang).toHaveBeenCalledWith('pl');
+    await menu.clickItem({ text: 'EN' });
+    expect(component.switchLang).toHaveBeenCalledWith('en');
   });
 });
